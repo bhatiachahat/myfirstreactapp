0840df7d70a25e70ab1169fa93a47cebeedecef2
bhatia100 <chahatbhatia2014@gmail.com>
2019-07-14 03:23:12 +0530

    Updates

---
+++ precache-manifest.bb449866f8b7f6dc562a550a7e2717ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38419f7cafa68673ff50a4df6c83fc1e",
+    "url": "/myfirstreactapp/index.html"
+  },
+  {
+    "revision": "94917ee021ba051ac1ef",
+    "url": "/myfirstreactapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/myfirstreactapp/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "94917ee021ba051ac1ef",
+    "url": "/myfirstreactapp/static/js/main.cc7c6913.chunk.js"
+  },
+  {
+    "revision": "e496e2481043da86ac8c",
+    "url": "/myfirstreactapp/static/js/runtime~main.3e200dc8.js"
+  }
+]);
